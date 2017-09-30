a7c83a28a1631263067e6d29e7bd6ea48ce4f6ce
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-09-30 17:24:34 +0900

    modify page_controller.ex

--- lib/parallel_serial_web/controllers/page_controller.ex
+++ lib/parallel_serial_web/controllers/page_controller.ex
@@ -4,4 +4,40 @@ defmodule ParallelSerialWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def create_requests(q) do
+  	[
+  		{:ated, "http://api.atnd.org/events/?keyword=#{q}&format=json"},
+  		{:connpass, "http://connpass.com/api/v1/event/?keyword=#{q}"},
+  		{:doorkeeper, "http://api.doorkeeper.jp/events/?q=#{q}"},
+  		{:zusaar, "http://www.zusaar.com/api/event/?keyword=#{q}"}
+  	]
+  end
+
+  def get_response(request) do
+  	{site, url} = request
+  	HTTPoison.start
+  	result = HTTPoison.get! url
+  	case result do
+  		%{status_code: 200, body: body} -> {site, Poison.decode!(body)}
+  		%{status_code: code} -> {site, nil}
+  	end
+  end
+
+  def parse_response(response) do
+  	case response do
+  		{:atnd, json} -> Map.get(json, "events") |> Enum.map(&Map.get(&1, "event"))
+  		{:connpass, json} -> Map.get(json, "events")
+  		{:doorkeeper, json} -> json |> Enum.map(&(Map.get(&1, "event")))
+  		{:zusaar, json} -> Map.get(json, "event")
+  	end
+  end
+
+  def serial_search(conn, %{"q" => q}) do
+  	results = create_requests(q)
+  	|> Enum.map(&get_response/1)
+  	|> Enum.map(&parse_response/1)
+  	|> List.flatten
+  	json conn, results
+  end
 end
